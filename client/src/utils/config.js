5d747ee6b2ca757018d746efc93530deedbe1511
mandy75444 <ms75444@gmail.com>
2025-09-08 10:11:22 +0530

    BAseURL

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://trg-1e4w.onrender.com/'
+export const BASE_URL = 'https://trg-1e4w.onrender.com/'
 
-export const BASE_URL = "http://localhost:5000/";
+// export const BASE_URL = "http://localhost:5000/";
 
